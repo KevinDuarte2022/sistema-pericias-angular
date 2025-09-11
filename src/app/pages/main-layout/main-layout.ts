65f3a6097f3dc320fa5ffa24a77412eed361aa9d
Kevin Duarte <elite.kevin.duarte@gmail.com>
2025-09-11 00:08:26 -0300

    feat: Sidebar agora com botão de hamburguer

--- src/app/pages/main-layout/main-layout.ts
+++ src/app/pages/main-layout/main-layout.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { RouterModule, RouterOutlet } from '@angular/router'
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterModule, RouterOutlet ],
+  imports: [RouterModule, RouterOutlet, CommonModule ],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css'
 })
 export class MainLayout {
+  isSidebarOpen = true; 
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
   constructor(public authService: AuthService) { }
 
   doLogout() {
