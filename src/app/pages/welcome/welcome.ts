34f66350da22e7d2cfe131ef852c8d33b427c45c
Kevin Duarte <elite.kevin.duarte@gmail.com>
2025-09-12 14:07:55 -0300

    Animation: Tela de Welcome com animação

--- src/app/pages/welcome/welcome.ts
+++ src/app/pages/welcome/welcome.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-welcome',
-  template: `
-    <h2 class="text-2xl font-bold mb-4">Bem-vindo de volta!</h2>
-    <p>Use o menu ao lado para navegar pelas seções do sistema.</p>
-  `,
+  standalone: true,
+  imports: [CommonModule, CardModule],
+  templateUrl: './welcome.html',
   styleUrls: ['./welcome.css']
 })
-export class Welcome {}
+export class Welcome implements AfterViewInit {
+
+  currentYear: number = new Date().getFullYear();
+
+
+  ngAfterViewInit() {
+    const blue = document.getElementById('blueOverlay')!;
+    const text = document.getElementById('welcomeText')!;
+    const bone = document.getElementById('bonequinho')!;
+
+    const animate = () => {
+      
+      blue.style.height = '0';
+      text.style.opacity = '0';
+      bone.style.opacity = '0';
+
+      // Anima camada azul
+      setTimeout(() => blue.classList.add('show-blue'), 200);
+
+      // Anima texto
+      setTimeout(() => text.classList.add('show-text'), 1500);
+
+      // Anima bonequinho
+      setTimeout(() => bone.classList.add('show-bone'), 2500);
+
+     
+      setTimeout(() => {
+        blue.classList.remove('show-blue');
+        text.classList.remove('show-text');
+        bone.classList.remove('show-bone');
+      }, 5000); 
+    };
+
+    animate(); 
+    setInterval(animate, 5200); 
+  }
+}
