c2f24a7f0b3b697274c69a2e765c70238b32fa5c
Kevin Duarte <elite.kevin.duarte@gmail.com>
2025-09-12 18:34:47 -0300

    feat(tests): adicionando teste de logout no MainLayout

--- src/app/pages/main-layout/main-layout.spec.ts
+++ src/app/pages/main-layout/main-layout.spec.ts
@@ -3,15 +3,21 @@ import { MainLayout } from './main-layout';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AuthService } from '../../services/auth.service';
+import { of } from 'rxjs';
 
 describe('MainLayout', () => {
   let component: MainLayout;
   let fixture: ComponentFixture<MainLayout>;
+  let authServiceSpy: jasmine.SpyObj<AuthService>;
 
   beforeEach(async () => {
+    
+    authServiceSpy = jasmine.createSpyObj('AuthService', ['logout', 'user']);
+    authServiceSpy.user.and.returnValue({ username: 'admin', roles: ['admin'] });
+
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule, HttpClientTestingModule, MainLayout],
-      providers: [AuthService],
+      providers: [{ provide: AuthService, useValue: authServiceSpy }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MainLayout);
@@ -21,5 +27,10 @@ describe('MainLayout', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call authService.logout when doLogout is called', () => {
+    component.doLogout();
+    expect(authServiceSpy.logout).toHaveBeenCalled();
   });
 });
