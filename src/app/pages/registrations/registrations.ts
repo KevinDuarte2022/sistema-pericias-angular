ace0ca78ba91d5b163e1015c0148adfe070a2c8f
Kevin Duarte <elite.kevin.duarte@gmail.com>
2025-09-12 21:15:22 -0300

    Style(Registration): Adicionando dialog nos botões para melhorar a experiência do usuário

--- src/app/pages/registrations/registrations.ts
+++ src/app/pages/registrations/registrations.ts
@@ -21,8 +21,13 @@ export class Registrations implements OnInit {
   showDeleteModal = false;
   currentSolicitacao: any = {};
   editingSolicitacao = false;
-  viewMode = false;   
+  viewMode = false;
   deletingSolicitacao: any;
+
+  alertDialogVisible = false;
+  alertMessage = '';
+  alertTitle = '';
+  alertSeverity: 'success' | 'warn' | 'info' | 'error' = 'info';
 
   constructor(private regService: RegistrationsService) { }
 
@@ -30,7 +35,7 @@ export class Registrations implements OnInit {
     this.loadSolicitacoes();
   }
 
-  
+
   loadSolicitacoes() {
     this.regService.getAll().subscribe({
       next: (res) => {
@@ -40,50 +45,55 @@ export class Registrations implements OnInit {
     });
   }
 
- 
-  saveSolicitacao() {
-    //To-do: Colocar um dialog dizendo que não pode salvar em modo visualização
-    if (this.viewMode) {
-      console.warn('Modo visualização - não pode salvar');
-      return;
-    }
+saveSolicitacao() {
 
-   
-    if (this.currentSolicitacao.hora && this.currentSolicitacao.hora.length === 5) {
-      this.currentSolicitacao.hora = this.currentSolicitacao.hora + ':00';
-    }
-
-    const request = this.editingSolicitacao
-      ? this.regService.update(this.currentSolicitacao.id, this.currentSolicitacao)
-      : this.regService.create(this.currentSolicitacao);
-
-    request.subscribe({
-      next: (res) => {
-        console.log('Salvo com sucesso:', res);
-
-        if (this.editingSolicitacao) {
-          const index = this.solicitacoes.findIndex(s => s.id === res.solicitacao.id);
-          if (index !== -1) {
-            this.solicitacoes[index] = res.solicitacao;
-          }
-        } else {
-          this.solicitacoes.unshift(res.solicitacao);
-        }
-
-        this.closeCard();
-      },
-      error: (err) => {
-        console.error('Erro ao salvar:', err);
-      }
-    });
+  if (!this.currentSolicitacao.data || !this.currentSolicitacao.hora || !this.currentSolicitacao.status) {
+    this.showAlert('Os campos Data, Hora e Status são obrigatórios!', 'Erro', 'error');
+    return;
   }
+
+  if (this.viewMode) {
+    this.showAlert('Não é possível salvar enquanto estiver em modo visualização!', 'Aviso', 'warn');
+    return;
+  }
+
+  if (this.currentSolicitacao.hora.length === 5) {
+    this.currentSolicitacao.hora += ':00';
+  }
+
+  const request = this.editingSolicitacao
+    ? this.regService.update(this.currentSolicitacao.id, this.currentSolicitacao)
+    : this.regService.create(this.currentSolicitacao);
+
+  request.subscribe({
+    next: (res) => {
+      console.log('Salvo com sucesso:', res);
+
+      if (this.editingSolicitacao) {
+        const index = this.solicitacoes.findIndex(s => s.id === res.solicitacao.id);
+        if (index !== -1) this.solicitacoes[index] = res.solicitacao;
+      } else {
+        this.solicitacoes.unshift(res.solicitacao);
+      }
+
+      this.closeCard();
+      this.showAlert('Registro salvo com sucesso!', 'Sucesso', 'success');
+    },
+    error: (err) => {
+      console.error('Erro ao salvar:', err);
+      this.showAlert(err?.error?.error || 'Erro ao salvar o registro!', 'Erro', 'error');
+    }
+  });
+}
+
+
 
 
   deleteSolicitacao() {
     this.regService.delete(this.deletingSolicitacao.id).subscribe({
       next: () => {
         console.log('Solicitação removida');
-       
+
         this.solicitacoes = this.solicitacoes.filter(s => s.id !== this.deletingSolicitacao.id);
         this.cancelDelete();
       },
@@ -126,7 +136,7 @@ export class Registrations implements OnInit {
   viewSolicitacao(sol: any) {
     this.currentSolicitacao = { ...sol };
     this.editingSolicitacao = false;
-    this.viewMode = true; 
+    this.viewMode = true;
     this.showCard = true;
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
@@ -135,4 +145,11 @@ export class Registrations implements OnInit {
     this.deletingSolicitacao = sol;
     this.showDeleteModal = true;
   }
+
+  showAlert(message: string, title: string = 'Aviso', severity: 'success' | 'warn' | 'info' | 'error' = 'info') {
+  this.alertMessage = message;
+  this.alertTitle = title;
+  this.alertSeverity = severity;
+  this.alertDialogVisible = true;
+}
 }
